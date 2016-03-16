593894d26c65c8c30d6b095eb17cdc2d33df7934
Chad Reitsma <chad@payvida.ca>
2016-03-15 21:09:52 -0600

    Initial Commit
    Quite possibly the easiest SDK you'll ever use!

---
+++ Payvida/gateway.php
@@ -0,0 +1,161 @@
+<?php
+/**
+  @package    PayVida::PHP::SDK
+  @author     Chad Reitsma
+  @copyright  Copyright 2016 PayVida Solutions Inc.
+  @version    v1.0
+*/
+
+namespace Payvida;	
+use Exception;
+
+
+
+class Gateway {
+	
+	
+	/**
+	 * Constructor
+	 * Set basic config vars + enpoint
+	 * 
+	 * @param string $merchantId Merchant ID
+	 * @param string $apiKey API Access Passcode
+	 */
+	 
+	public function __construct($merchantId, $apiKey, $endpoint) {
+		
+		# Make sure cURL is installed
+		if ( !extension_loaded('curl'))  throw new Exception('The cURL extension is required (http://php.net/manual/en/book.curl.php)');
+		
+		# Check required vars
+		if (!$merchantId)	throw new Exception('merchantId is missing');
+		if (!$apiKey)	throw new Exception('apiKey is missing');
+		
+		$this->merchantId = $merchantId;
+		$this->apiKey = $apiKey;
+		$this->endpoint = $endpoint;
+		
+	}
+	
+	
+	public function ccGetToken($data)  {
+		$data['action'] = "ccgettoken";
+		return $this->request($data);
+	}
+	
+	public function ccUpdateToken($data)  {
+		$data['action'] = "ccupdatetoken";
+		return $this->request($data);
+	}
+	
+	
+	
+	public function ccSale($data, $amount='')  {
+		
+		
+		if (!$amount) throw new Exception('Amount cannot be empty when attempting to process a transaction!');
+		$data['amount'] = $amount;
+		
+		
+		//If using a token, unset any cc vars they may have sent by mistake
+		if (!empty($data['token'])) {
+			unset($data['card_number']);
+			unset($data['card_name']);
+			unset($data['card_expiry_date']);
+			unset($data['card_cvv']);
+		}
+		
+		$data['action'] = "ccsale";	
+		return $this->request($data);
+	}
+	
+	
+	
+	public function ccAuthOnly($data, $amount='')  {
+		
+		
+		if (!$amount) throw new Exception('Amount cannot be empty when performing a pre-authorization!');
+		$data['amount'] = $amount;
+		
+		
+		//If using a token, unset any cc vars they may have sent by mistake
+		if (!empty($data['token'])) {
+			unset($data['card_number']);
+			unset($data['card_name']);
+			unset($data['card_expiry_date']);
+			unset($data['card_cvv']);
+		}
+		
+		$data['action'] = "ccauthonly";	
+		return $this->request($data);
+	}
+		
+	
+	
+	public function ccComplete($txn_id)  {
+		
+		$data['txn_id'] = $txn_id;
+		$data['action'] = "cccomplete";	
+		
+		return $this->request($data);
+	}
+	
+	public function ccReturn($txn_id)  {
+		
+		$data['txn_id'] = $txn_id;
+		$data['action'] = "ccreturn";	
+		
+		return $this->request($data);
+	}
+	
+	public function ccVoid($txn_id)  {
+		
+		$data['txn_id'] = $txn_id;
+		$data['action'] = "ccvoid";	
+		
+		return $this->request($data);
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Sends request payload to API via CURL
+	 * 
+	 * @param array $data (transaction data + action added in function request)
+	 */
+	private function request($data) {
+		
+		
+		$data['merchantid'] = $this->merchantId;
+		$data['apikey'] = $this->apiKey;
+		
+
+		$ch = curl_init();    								
+		curl_setopt($ch, CURLOPT_URL, $this->endpoint); 	
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); 		
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30); 				
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 		
+		
+		$result = curl_exec($ch); 							
+		
+		if (curl_errno($ch)) {
+		   throw new Exception('Communication error, endpoint: ' . $this->endpoint . " is unavailable at this time.");
+		} else {
+		   curl_close($ch);
+		}
+		
+		return $result;
+		
+	}
+	
+	
+
+}
+
+
+
+?>
---
+++ example.php
@@ -0,0 +1,177 @@
+<?php
+
+/**
+  @package    PayVida::PHP::SDK
+  @author     Chad Reitsma
+  @copyright  Copyright 2016 PayVida Solutions Inc.
+  @version    v1.0
+  
+  
+  This file contains examples of all API functions
+  
+*/
+
+
+# Fill in your Dev or Live Credentials
+$merchantId = '';
+$apiKey = '';
+$gatewayUrl = 'https://demo.payvida.ca/api/';  
+
+
+require_once('Payvida/gateway.php');
+
+
+# Initiatialize PayVida Object
+$payvida = new \Payvida\Gateway($merchantId, $apiKey, $gatewayUrl);
+
+
+
+# Data for Credit-card payment
+$payment_card_data = array(
+					  
+					'card_name' => 'Test Card',
+					'card_number' => '6011500080009080',
+					'card_expiry_date' => '0620',
+					'card_cvv' => '123',
+					
+					# I've included billing fields but they are not required
+					'billing_name'  => 'Test Testerson',
+					'billing_company_name'  => 'PayVida',
+					'billing_address1'  => '123 Fake Street',
+					'billing_address2' => '',
+					'billing_postal_zip' => 'T2Y 4M3',
+					'billing_city' => 'Kelowna',
+					'billing_province_state' => 'BC',
+					'billing_country' => 'CAN',
+					'billing_phone' => '555-123-4567',
+					'billing_email' => 'totallyvalid@emailaddress.com',
+					
+					# I've included shipping fields but they are not required
+					'shipping_name'  => 'Hank Scorpio',
+					'shipping_company_name'  => 'Globex Corp.',
+					'shipping_address1'  => '1111 Volcano Lair',
+					'shipping_address2' => '',
+					'shipping_postal_zip' => 'T1X CB4',
+					'shipping_city' => 'Cypress Creek',
+					'shipping_province_state' => 'BC',
+					'shipping_country' => 'CAN',
+					'shipping_phone' => '555-123-45678',
+					'shipping_email' => 'hank@globexcorp.com'
+					
+				);
+
+
+
+# Data for Tokenization 
+$get_token_data =  array(
+					'card_name' => 'Test Card',
+					'card_number' => '6011500080009080',
+					'card_expiry_date' => '0620',
+					'card_cvv' => '123',
+					
+					# I've included billing fields but they are not required
+					'billing_name'  => 'Test Testerson',
+					'billing_company_name'  => 'PayVida',
+					'billing_address1'  => '123 Fake Street',
+					'billing_address2' => '',
+					'billing_postal_zip' => 'T2Y 4M3',
+					'billing_city' => 'Kelowna',
+					'billing_province_state' => 'BC',
+					'billing_country' => 'CAN',
+					'billing_phone' => '555-123-4567',
+					'billing_email' => 'totallyvalid@emailaddress.com'
+					);
+
+
+# Data for Update token
+$token_update_data = array(
+							'card_token' => '9318222095339080',  
+							'card_expiry_date' => '0320'
+					  );
+
+# Data for Token Payment
+$payment_token_data = array(
+							'card_token' => '9318222095339080'
+					  );
+
+
+
+
+
+
+
+
+
+
+/*  EXAMPLE FUNCTIONS */
+
+
+# Get Token
+//$result = $payvida->ccgettoken($get_token_data);   
+
+
+# Update Token with the new Expiry Date
+//$result = $payvida->ccupdatetoken($token_update_data);   
+
+
+
+# Make Credit-Card Payment for $25.00
+//$amount = "25.00";
+//$result = $payvida->ccsale($payment_card_data, $amount);   
+
+# Pre-Auth a Credit-Card for $25.00
+//$amount = "25.00";
+//$result = $payvida->ccauthonly($payment_card_data, $amount);   
+
+
+
+# Make Payment with a Token for $99.00
+//$amount = "99.00";
+//$result = $payvida->ccsale($payment_token_data, $amount);   
+
+# Make Pre-Auth with a Token for $99.00
+//$amount = "99.00";
+//$result = $payvida->ccauthonly($payment_token_data, $amount);   
+
+
+
+# Complete a Pre-auth  (Use the transaction ID you got from the Make pre-auth function above)
+//$txn_id = '150316A15-18EA7730-C671-40E0-BB96-958DEEC5A3D0';
+//$result = $payvida->cccomplete($txn_id);
+
+
+
+# Return a Transaction
+//$txn_id = '150316A15-18EA7730-C671-40E0-BB96-958DEEC5A3D0';
+//$result = $payvida->ccreturn($txn_id);
+
+
+# Void a Transaction 
+//$txn_id = '150316A15-18EA7730-C671-40E0-BB96-958DEEC5A3D0';
+//$result = $payvida->ccvoid($txn_id);
+
+
+
+
+
+/* PARSE RESULTS */
+
+$response = json_decode($result, true);  # Convert JSON response into array for ease of use
+if ($response['code'] >= 400) { 
+	
+	# Declined/Error
+	echo "There was an error: " . $response['message'];
+	exit();
+	
+}
+else {
+	
+	# Success!
+	# Print the array (response) so you can see the key value pairs that are returned
+	print_r($response);
+	
+
+}
+
+
+?>
